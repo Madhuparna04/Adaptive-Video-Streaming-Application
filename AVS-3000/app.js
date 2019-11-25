b5b2d48f76692e7eadc3fe1775b436685bf50c1e
Apoorva <Apoorva>
2019-11-26 00:15:26 +0530

    all fixes

--- AVS-3000/app.js
+++ AVS-3000/app.js
@@ -40,11 +40,6 @@ app.use(function(err, req, res, next) {
   console.log("Some error");
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  /*
-  res.status(err.status || 500);
-  res.render('error');
-  */
- // console.log(err);
 });
 
 app.on('listening', function () {
